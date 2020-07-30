8622824a8ba908140db4aab688626e5a0d93a0c5
davien21 <chidiebereekennia@gmail.com>
2020-07-30 01:23:32 +0100

    first commit

---
+++ assets/js/MovieClass.js
@@ -0,0 +1,36 @@
+/*Movie Class by Chidiebere Ekennia*/
+class MovieClass {
+	constructor () {
+		this.movie_array = movie_array;
+		// this.movie_array.map((movie) =>{movie.rented = false});
+		this.store_owner = 'Chidiebere Ekennia'
+	}
+	getMovieArray () {
+		return this.movie_array;
+	}
+	searchMoviesByString (string) {
+		string = string.toLowerCase();
+		let movie_array = this.movie_array;
+		let filtered_array = [];
+		movie_array.forEach(function(movie_obj) {
+
+			let values_array = Object.values(movie_obj);
+
+			values_array.forEach(function(value) {
+
+				value = value.toLowerCase();
+
+				if (value.includes(string) && !filtered_array.includes(movie_obj)) {
+
+					filtered_array.push(movie_obj);
+
+				}
+
+			});
+
+		});
+		return filtered_array;
+	}
+}
+
+// console.log(movie_array);
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+let movie_store = new MovieClass;
+let movies = movie_store.getMovieArray();
+
+let search_btn = document.querySelector('#seach-btn')
+let return_btn = document.querySelector('#return-btn')
+let search_input = document.querySelector('input#movie-input')
+let movie_ = document.querySelector('table#unrented');
+let movie_table = movie_.querySelector('tbody');
+
+let movie_row = (movie_obj,index) => {
+	let html = 
+	`
+		<tr>
+			<td class="name">${movie_obj.name}</td>
+			<td>${movie_obj.genre}</td>
+			<td class="director">${movie_obj.director}</td>
+			<td>
+				<button class="btn btn-success btn-block" onclick ="rent(this)">Rent</button>
+			</td>
+
+		</tr>
+	`;
+	return html;
+}
+
+function makeMovieTable (list_array) {
+	let movie_list = ''
+	list_array.forEach((movie_obj,index) => {
+		movie_list+=movie_row(movie_obj,index)
+	});
+	movie_table.innerHTML = movie_list;
+}
+
+makeMovieTable(movies);
+function rent (btn) {
+	let movie_row = btn.closest('tr');
+	let movie_name = movie_row.querySelector('.name')
+	let movie_director = movie_row.querySelector('.director')
+	alert(`You have successfully rented the Movie: \n${movie_name.innerText}\n By \n${movie_director.innerText}`);
+	rentMovie(movie_row);
+}
+
+search_input.oninput = (e) => {
+	let string = e.target.value;
+	let movie_list = movie_store.searchMoviesByString(string);
+	makeMovieTable(movie_list)
+}
+
+return_btn.onclick = () => {
+	let all_rows = document.querySelectorAll('tr');
+	all_rows.forEach( function(element) {
+		element.classList.remove('d-none')
+	});
+}
+
+function rentMovie (table_row) {
+	table_row.classList.add('d-none');
+}
+
+
